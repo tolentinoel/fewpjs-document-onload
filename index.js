a378425a22624c7c46a733f61ef108c76c751e5e
tolentinoel <rckrleyn@gmail.com>
2020-11-16 16:07:53 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    targetParagraph();
+  });
+
+//   console.log(
+//     "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+//   );
+ 
+  function targetParagraph() {
+      document.getElementById('text').innerHTML = "This is really cool!"
+  }
